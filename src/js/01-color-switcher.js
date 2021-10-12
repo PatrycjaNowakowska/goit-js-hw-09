22e2a64d7eb01a3680e503db0033cf3ae110d0af
PatrycjaNowakowska <poczta.pnowakowska@gmail.com>
2021-10-12 17:56:14 +0200

    zad 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+//Aby wygenerować losowy kolor użyj funkcji
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+stopBtn.disabled = true;
+
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(bgColorChanger, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  
+});
+
+stopBtn.addEventListener('click', () => {
+    clearTimeout(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  
+});
+
+const bgColorChanger = () => {
+  body.style.backgroundColor = getRandomHexColor();
+};
